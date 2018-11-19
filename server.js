bd1a52439ab0e08a237ee707a8c8ac006b393485
ajay-arya <ajayarya512@gmail.com>
2018-11-19 21:46:57 +0530

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// var { mongoose } = require('./mongoDB/database');
-// var { user } = require('./models/user');
-// var { review } = require('./models/reviews');
+var { mongoose } = require('./mongoDB/database');
+var { user } = require('./models/user');
+var { review } = require('./models/reviews');
 
 var chat = express();
 chat.use(bodyParser.json());
