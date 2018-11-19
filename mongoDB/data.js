ddab0f27c5afbbccfe3401b2732bce91cd08fc42
ajay-arya <ajayarya512@gmail.com>
2018-11-20 00:17:51 +0530

    heroku

--- mongoDB/data.js
+++ mongoDB/data.js
@@ -1,4 +1,4 @@
-mongoUrl = "mongodb://localhost:27017/chatbot";
-// mongoUrl = "mongodb+srv://ajay:ajay123@user-iiwhv.mongodb.net/test?retryWrites=true";
+// mongoUrl = "mongodb://localhost:27017/chatbot";
+mongoUrl = "mongodb+srv://ajay:ajay123@user-iiwhv.mongodb.net/test?retryWrites=true";
 
 module.exports.mongoUrl = mongoUrl;
